d2981b9c08e7af9c4b1a7c517006731d5f311351
Henrique Raupp Bobsin <hraupp2@gmail.com>
2023-11-28 22:52:15 -0300

    Added a states machine

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -26,6 +26,8 @@
 /* USER CODE BEGIN Includes */
 
 #include "math.h"
+#include "vetor_senoide.h"
+
 
 /* USER CODE END Includes */
 
@@ -38,7 +40,6 @@
 /* USER CODE BEGIN PD */
 
 #define DOISPI 6.2831
-#define TAM_SIN 250 // 15Khz
 #define MOV_ENCODER 25
 
 /* USER CODE END PD */
@@ -52,29 +53,26 @@
 
 /* USER CODE BEGIN PV */
 
-uint32_t sin_wave[TAM_SIN] = { 100, 102, 105, 107, 110, 112, 115, 117, 119, 122,
-		124, 127, 129, 132, 134, 136, 139, 141, 143, 145, 148, 150, 152, 154,
-		156, 158, 160, 162, 164, 166, 168, 170, 172, 173, 175, 177, 178, 180,
-		181, 183, 184, 185, 187, 188, 189, 190, 191, 192, 193, 194, 195, 195,
-		196, 197, 197, 198, 198, 199, 199, 199, 199, 199, 199, 199, 199, 199,
-		199, 199, 199, 198, 198, 197, 197, 196, 195, 195, 194, 193, 192, 191,
-		190, 189, 188, 187, 185, 184, 183, 181, 180, 178, 177, 175, 173, 172,
-		170, 168, 166, 164, 162, 160, 158, 156, 154, 152, 150, 148, 145, 143,
-		141, 139, 136, 134, 132, 129, 127, 124, 122, 119, 117, 115, 112, 110,
-		107, 105, 102, 100, 97, 94, 92, 89, 87, 84, 82, 80, 77, 75, 72, 70, 67,
-		65, 63, 60, 58, 56, 54, 51, 49, 47, 45, 43, 41, 39, 37, 35, 33, 31, 29,
-		27, 26, 24, 22, 21, 19, 18, 16, 15, 14, 12, 11, 10, 9, 8, 7, 6, 5, 4, 4,
-		3, 2, 2, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 2, 2, 3, 4, 4,
-		5, 6, 7, 8, 9, 10, 11, 12, 14, 15, 16, 18, 19, 21, 22, 24, 26, 27, 29,
-		31, 33, 35, 37, 39, 41, 43, 45, 47, 49, 51, 54, 56, 58, 60, 63, 65, 67,
-		70, 72, 75, 77, 80, 82, 84, 87, 89, 92, 94, 97 };
+
+
+///////////////////// Maquina de estados//////////////////////
+enum {
+
+	desligado = 0, ligado, bypass
+
+};
+
+uint8_t estado_atual = desligado;
+/////////////////////////////////////////////////////////////////////
 
 uint32_t sin_wave_mod[TAM_SIN];
-//uint32_t sin_wave[TAM_SIN];
 uint16_t i;
 uint16_t cont = 0;
+
+///////////////////ENCODER///////////////
 uint16_t contador_encoder = 0;
 uint8_t variacao_encoder = 0;
+/////////////////////////////////////////
 
 /* USER CODE END PV */
 
@@ -102,9 +100,8 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
 		}
 
 		//__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, sin_wave_mod[cont]);
-	   //__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_2, sin_wave_mod[cont]);
+		//__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_2, sin_wave_mod[cont]);
 		__HAL_TIM_SET_COMPARE(&htim1, TIM_CHANNEL_1, sin_wave[cont]);
-
 
 		__HAL_TIM_SET_PRESCALER(&htim10, variacao_encoder);
 
@@ -117,123 +114,170 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim) {
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
-int main(void)
-{
-  /* USER CODE BEGIN 1 */
+ * @brief  The application entry point.
+ * @retval int
+ */
+int main(void) {
+	/* USER CODE BEGIN 1 */
 
-  /* USER CODE END 1 */
+	/* USER CODE END 1 */
 
-  /* MCU Configuration--------------------------------------------------------*/
+	/* MCU Configuration--------------------------------------------------------*/
 
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
+	/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+	HAL_Init();
 
-  /* USER CODE BEGIN Init */
+	/* USER CODE BEGIN Init */
 
-  /* USER CODE END Init */
+	/* USER CODE END Init */
 
-  /* Configure the system clock */
-  SystemClock_Config();
+	/* Configure the system clock */
+	SystemClock_Config();
 
-  /* USER CODE BEGIN SysInit */
+	/* USER CODE BEGIN SysInit */
 
-  /* USER CODE END SysInit */
+	/* USER CODE END SysInit */
 
-  /* Initialize all configured peripherals */
-  MX_GPIO_Init();
-  MX_USART2_UART_Init();
-  MX_TIM1_Init();
-  MX_TIM10_Init();
-  MX_TIM4_Init();
-  /* USER CODE BEGIN 2 */
-
-	HAL_TIM_Base_Start_IT(&htim10);
-	HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
-	HAL_TIMEx_PWMN_Start(&htim1, TIM_CHANNEL_1);
-	//HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_2);
-
-
-	/////////Start Encoder//////////////
-	HAL_TIM_Encoder_Start(&htim4, TIM_CHANNEL_1);
-	HAL_TIM_Encoder_Start(&htim4, TIM_CHANNEL_2);
-
-	/*
-	 for (i = 0; i < TAM_SIN; i++) {
-
-	 sin_wave[i] = ((sin(i * (DOISPI / TAM_SIN))) + 1) * ((200 + 1) / 2);
-	 }
-
-	 */
+	/* Initialize all configured peripherals */
+	MX_GPIO_Init();
+	MX_USART2_UART_Init();
+	MX_TIM1_Init();
+	MX_TIM10_Init();
+	MX_TIM4_Init();
+	/* USER CODE BEGIN 2 */
 
 //////////////////DIVIDE SENOIDE////////////////////////
 	/*
-	for (int j = 0; j < TAM_SIN; j++) {
+	 for (int j = 0; j < TAM_SIN; j++) {
 
-		sin_wave_mod[j] = sin_wave[j] / 2;
+	 sin_wave_mod[j] = sin_wave[j] / 2;
 
-	}
-	*/
+	 }
+	 */
 
-  /* USER CODE END 2 */
+	/* USER CODE END 2 */
 
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
+	/* Infinite loop */
+	/* USER CODE BEGIN WHILE */
 	while (1) {
-    /* USER CODE END WHILE */
 
-    /* USER CODE BEGIN 3 */
+	//============================= Maquina de estados =============================//
+
+
+		switch (estado_atual) {
+
+		case desligado:
+
+			HAL_TIM_Base_Stop_IT(&htim10);
+			HAL_TIM_PWM_Stop(&htim1, TIM_CHANNEL_1);
+			HAL_TIMEx_PWMN_Stop(&htim1, TIM_CHANNEL_1);
+
+			/////////Start Encoder//////////////
+			HAL_TIM_Encoder_Stop(&htim4, TIM_CHANNEL_1);
+			HAL_TIM_Encoder_Stop(&htim4, TIM_CHANNEL_2);
+
+			//Deixa resistor ligado
+			//Desliga led
+
+			//if(botao){
+			//estado_atual = ligado
+			//}
+
+			break;
+
+		case ligado:
+
+			HAL_TIM_Base_Start_IT(&htim10);
+			HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
+			HAL_TIMEx_PWMN_Start(&htim1, TIM_CHANNEL_1);
+
+			/////////Start Encoder//////////////
+			HAL_TIM_Encoder_Start(&htim4, TIM_CHANNEL_1);
+			HAL_TIM_Encoder_Start(&htim4, TIM_CHANNEL_2);
+
+			//resistor ligado
+			//liga led de circuito ligado
+			//led de bypass desligado
+
+			//if(tempo >= 3){
+			//estado_atual = bypass
+			//}
+
+			break;
+
+		case bypass:
+
+			//aciona o bypass
+			//liga led de bypass
+
+			break;
+
+		default:
+
+			HAL_TIM_Base_Stop_IT(&htim10);
+			HAL_TIM_PWM_Stop(&htim1, TIM_CHANNEL_1);
+			HAL_TIMEx_PWMN_Stop(&htim1, TIM_CHANNEL_1);
+
+			/////////Start Encoder//////////////
+			HAL_TIM_Encoder_Stop(&htim4, TIM_CHANNEL_1);
+			HAL_TIM_Encoder_Stop(&htim4, TIM_CHANNEL_2);
+
+			//Deixa resistor ligado
+			//Desliga led
+
+			break;
+
+		}
+
+		/* USER CODE END WHILE */
+
+		/* USER CODE BEGIN 3 */
 	}
-  /* USER CODE END 3 */
+	/* USER CODE END 3 */
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
-void SystemClock_Config(void)
-{
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+ * @brief System Clock Configuration
+ * @retval None
+ */
+void SystemClock_Config(void) {
+	RCC_OscInitTypeDef RCC_OscInitStruct = { 0 };
+	RCC_ClkInitTypeDef RCC_ClkInitStruct = { 0 };
 
-  /** Configure the main internal regulator output voltage
-  */
-  __HAL_RCC_PWR_CLK_ENABLE();
-  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE3);
+	/** Configure the main internal regulator output voltage
+	 */
+	__HAL_RCC_PWR_CLK_ENABLE();
+	__HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE3);
 
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
-  RCC_OscInitStruct.PLL.PLLM = 16;
-  RCC_OscInitStruct.PLL.PLLN = 336;
-  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
-  RCC_OscInitStruct.PLL.PLLQ = 2;
-  RCC_OscInitStruct.PLL.PLLR = 2;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
+	/** Initializes the RCC Oscillators according to the specified parameters
+	 * in the RCC_OscInitTypeDef structure.
+	 */
+	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+	RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+	RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+	RCC_OscInitStruct.PLL.PLLM = 16;
+	RCC_OscInitStruct.PLL.PLLN = 336;
+	RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
+	RCC_OscInitStruct.PLL.PLLQ = 2;
+	RCC_OscInitStruct.PLL.PLLR = 2;
+	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+		Error_Handler();
+	}
 
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+	/** Initializes the CPU, AHB and APB buses clocks
+	 */
+	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
+			| RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
-  {
-    Error_Handler();
-  }
+	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK) {
+		Error_Handler();
+	}
 }
 
 /* USER CODE BEGIN 4 */
@@ -241,17 +285,16 @@ void SystemClock_Config(void)
 /* USER CODE END 4 */
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
-void Error_Handler(void)
-{
-  /* USER CODE BEGIN Error_Handler_Debug */
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
+void Error_Handler(void) {
+	/* USER CODE BEGIN Error_Handler_Debug */
 	/* User can add his own implementation to report the HAL error return state */
 	__disable_irq();
 	while (1) {
 	}
-  /* USER CODE END Error_Handler_Debug */
+	/* USER CODE END Error_Handler_Debug */
 }
 
 #ifdef  USE_FULL_ASSERT
